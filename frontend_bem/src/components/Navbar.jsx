384fa895440cbdc4f33b85d96ae9c1b17fc375e2
betu55 <61617248+betu55@users.noreply.github.com>
2026-08-18 17:01:21 -0400

    Add 'About Me' link for admin users in Navbar

--- frontend_bem/src/components/Navbar.jsx
+++ frontend_bem/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   const navbarLinks = isAdmin ? 
-  ["Home", "Projects", "Experience", "Contact", "Tracker"]:
+  ["Home", "About Me", "Projects", "Experience", "Contact", "Tracker"]:
   ["Home", "Projects", "Experience", "Contact"];
 
   const handleLogout = (isAdmin) => {
